fa6945d51443e584143d67acde937b4a59708bac
lshpaner <lshpaner@ucla.edu>
2024-07-29 08:41:38 -0700

    updated conf.py file based on prior configs from other projects

--- source/conf.py
+++ source/conf.py
@@ -16,10 +16,25 @@ release = "0.0.1b0"
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx.ext.todo",
     "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    # "sphinxcontrib.bibtex",
 ]
+
+# Add this line to specify the bibliography file
+# bibtex_bibfiles = ["references.bib"]
+
+extensions.append("sphinx.ext.intersphinx")
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
+
+copybutton_prompt_text = r">|\$ "
+copybutton_prompt_is_regexp = True
 
 
 templates_path = ["_templates"]
@@ -31,3 +46,11 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# If your documentation is served from a subdirectory, set this to the subdirectory path
+html_show_sourcelink = False
+
+
+# def setup(app):
+#     app.add_css_file("custom.css")
+#     app.add_js_file("custom.js")
