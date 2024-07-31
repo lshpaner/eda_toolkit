de9c72bc26c0a1acaf6ec36ebda7c51d9fb1f8d4
lshpaner <lshpaner@ucla.edu>
2024-07-30 22:30:14 -0700

    removed dirs that belong only in docs branch

--- docs/_static/custom.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const images = document.querySelectorAll('.no-click img');
-    images.forEach((img) => {
-        img.style.pointerEvents = 'none';
-    });
-});
