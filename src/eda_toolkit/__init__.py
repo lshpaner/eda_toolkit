566ba741af25af329fa46ca335086bbfe8a32a6a
lshpaner <lshpaner@ucla.edu>
2024-09-08 09:14:03 -0700

    alpha release - new pdp functions, updated __init__ params

--- src/eda_toolkit/__init__.py
+++ src/eda_toolkit/__init__.py
@@ -1,14 +1,14 @@
-__version__ = "0.0.7"
+"""
+EDA Toolkit - A Python library for EDA, including visualizations, directory 
+management, data preprocessing, reporting, and more.
+
+Version: 0.0.8a
+"""
+
+__version__ = "0.0.8a"
 __author__ = "Leonid Shpaner, Oscar Gil"
 __email__ = "lshpaner@ucla.edu; info@oscargildata.com"
 
-
-"""
-EDA Toolkit - A Python library for EDA, including visualizations, 
-directory management, data preprocessing, reporting, and more.
-
-Version: 0.0.7
-"""
 
 from .main import *
 from .ml_eda import *
