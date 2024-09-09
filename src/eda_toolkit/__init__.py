0b1963f9e0536697a138657256860155b99eeefe
lshpaner <lshpaner@ucla.edu>
2024-09-08 19:51:08 -0700

    pdf_plots:  Minor bug fixes, enhancements, and testing improvements.

--- src/eda_toolkit/__init__.py
+++ src/eda_toolkit/__init__.py
@@ -2,10 +2,10 @@
 EDA Toolkit - A Python library for EDA, including visualizations, directory 
 management, data preprocessing, reporting, and more.
 
-Version: 0.0.8b
+Version: 0.0.8c
 """
 
-__version__ = "0.0.8b"
+__version__ = "0.0.8c"
 __author__ = "Leonid Shpaner, Oscar Gil"
 __email__ = "lshpaner@ucla.edu; info@oscargildata.com"
 
