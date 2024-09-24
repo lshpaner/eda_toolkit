d441b488143870abbb15b6178a73921bb05c270c
lshpaner <lshpaner@ucla.edu>
2024-09-24 14:58:46 -0700

    versioned up to 0.0.11 in prep for new release

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@ from docutils.parsers.rst import roles
 project = "EDA Toolkit"
 copyright = "2024, Leonid Shpaner, Oscar Gil"
 author = "Leonid Shpaner, Oscar Gil"
-release = "0.0.10"
+release = "0.0.11"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
