af0033b236f27be64d864f19215eccc10b46f4bb
lshpaner <lshpaner@ucla.edu>
2024-10-18 20:26:53 -0700

    updated docstring, + plot save, + boxcox, + error-handling, + versions in setup.py

--- setup.py
+++ setup.py
@@ -23,16 +23,17 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],  # Classifiers for the package
-    python_requires=">=3.7.4",  # Minimum version of Python required
+    python_requires=">=3.7.4, <3.12",  # Python versions 3.7.4 to 3.11
     install_requires=[
-        "jinja2>=3.1.4",  # Minimum version of jinja2 required
-        "matplotlib>=3.5.3",  # Minimum version of matplotlib required
-        "nbformat>=4.2.0",  # Minimum version of nbformat required
-        "numpy>=1.21.6",  # Minimum version of numpy required
-        "pandas>=1.3.5",  # Minimum version of pandas required
-        "plotly>=5.18.0",  # Minimum version of plotly required
-        "scikit-learn>=1.0.2",  # Minimum version of scikit-learn required
-        "seaborn>=0.12.2",  # Minimum version of seaborn required
-        "xlsxwriter>=3.2.0",  # Minimum version of xlsxwriter required
+        "jinja2==3.1.4",  # Exact version of Jinja2 required
+        "matplotlib>=3.5.3,<=3.9.2",  # Matplotlib capped at 3.9.2
+        "nbformat>=4.2.0,<=5.10.4",  # Nbformat capped at 5.10.4
+        "numpy>=1.21.6,<=2.1.0",  # Numpy capped at 2.1.0
+        "pandas>=1.3.5,<=2.2.2",  # Pandas capped at 2.2.2
+        "plotly>=5.18.0,<=5.24.0",  # Plotly capped at 5.24.0
+        "scikit-learn>=1.0.2,<=1.5.2",  # Scikit-learn capped at 1.5.2
+        "seaborn>=0.12.2,<0.13.0",  # Seaborn capped below 0.13.0
+        "xlsxwriter>=3.2.0,<=3.2.0",  # XlsxWriter capped at 3.2.0
+        "scipy>=1.7.3,<=1.11.1",  # SciPy capped at 1.11.1
     ],
 )
