ae4290e7d7bf525131cbc3e81a070f13f1b78469
lshpaner <lshpaner@ucla.edu>
2024-10-27 22:15:49 -0700

    versioned to 0.0.11a3

--- src/eda_toolkit/__init__.py
+++ src/eda_toolkit/__init__.py
@@ -1,11 +1,40 @@
-"""
-EDA Toolkit - A Python library for EDA, including visualizations, directory 
-management, data preprocessing, reporting, and more.
+""" 
 
-Version: 0.0.11a2
++--------------------------------------------------------------------------------+
+|  _____   ____       _              _____                   _   _      _   _    |
+| | ____| |  _ \     / \            |_   _|   ___     ___   | | | | __ (_) | |_  |
+| |  _|   | | | |   / _ \    _____    | |    / _ \   / _ \  | | | |/ / | | | __| |
+| | |___  | |_| |  / ___ \  |_____|   | |   | (_) | | (_) | | | |   <  | | | |_  |
+| |_____| |____/  /_/   \_\           |_|    \___/   \___/  |_| |_|\_\ |_|  \__| |
+|                                                                                |
++--------------------------------------------------------------------------------+                                                                                                                                                            
+
+Welcome to EDA Toolkit, a collection of utility functions designed to streamline 
+your exploratory data analysis (EDA) tasks. This repository offers tools for 
+directory management, some data preprocessing, reporting, visualizations, and 
+more, helping you efficiently handle various aspects of data manipulation and 
+analysis.
+
+PyPI: https://pypi.org/project/eda-toolkit/
+Documentation: https://lshpaner.github.io/eda_toolkit
+
+Acknowledgements
+
+We would like to express our deepest gratitude to Dr. Ebrahim Tarshizi, our 
+mentor during our time in the University of San Diego M.S. Applied Data Science 
+Program. His unwavering dedication and mentorship played a pivotal role in our 
+academic journey, guiding us to successfully graduate from the program and 
+pursue successful careers as data scientists.
+
+We also extend our thanks to the Shiley-Marcos School of Engineering at the 
+University of San Diego for providing an exceptional learning environment and 
+supporting our educational endeavors.
+
+
+Version: 0.0.11a3
 """
 
-__version__ = "0.0.11a2"
+__version__ = "0.0.11a3"
 __author__ = "Leonid Shpaner, Oscar Gil"
 __email__ = "lshpaner@ucla.edu; info@oscargildata.com"
 
