76d821fa5c805f79b767829658b6b6934180df89
Oscar-Gil-Data <gil.oscar@gmail.com>
2025-12-14 17:18:40 -0800

    ECDF example script and added scipy import to plot.py

---
+++ py_example_scripts/ecdf_plot_integration_in_data_doctor.py
@@ -0,0 +1,38 @@
+# %%
+################################################################################
+##################### ECDF Plot Integration in data_doctor #####################
+################################################################################
+
+from ucimlrepo import fetch_ucirepo
+from eda_toolkit import data_doctor
+
+# %%
+################################################################################
+## UCI ML Repository
+################################################################################
+
+# fetch dataset
+adult = fetch_ucirepo(id=2)
+
+# data (as pandas dataframes)
+X = adult.data.features
+y = adult.data.targets
+
+# Combine X and y into entire df
+adult_df = X.join(y, how="inner")
+
+# %%
+################################################################################
+## data_doctor with ECDF
+################################################################################
+
+print("\nRunning data_doctor with plot_type=['kde', 'ecdf', 'box_violin'] ...\n")
+
+data_doctor(
+    df=adult_df,
+    feature_name="age",
+    plot_type=["kde", "ecdf", "box_violin"],
+    scale_conversion="log",
+)
+
+
