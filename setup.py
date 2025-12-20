ec5eef5ae9ecce133bc6d8f4fd382e6e145e5f33
lshpaner <lshpaner@ucla.edu>
2025-12-20 10:43:07 -0800

    version up to 0.0.21

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eda_toolkit",
-    version="0.0.20",
+    version="0.0.21",
     author="Leonid Shpaner, Oscar Gil",
     author_email="lshpaner@ucla.edu",
     description="A Python library for EDA, including visualizations, directory management, data preprocessing, reporting, and more.",
